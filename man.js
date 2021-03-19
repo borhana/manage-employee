2b833e613e7dcb3058fd227487f76fc8da3b710f
borhana <boranamihaj2@gmail.com>
2021-03-19 20:33:02 +0100

    menu, add new employee

--- man.js
+++ man.js
@@ -15,4 +15,37 @@ function openDeleteModal(){
     modalDiv.style.display = "block";
     modalDiv.classList.remove("fade");
     // console.log(modalDiv);
+} 
+
+function menuFunction() {
+    var menu = document.getElementById("menuLinks");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', (event) =>{
+    console.log(event);
+    document.getElementById('addModalButton').addEventListener('click',openAddModal);
+  })
+
+  function openAddModal(){
+    const modalAdd = document.getElementById('addModal');
+    modalAdd.style.display = "block";
+    modalAdd.classList.remove("fade");
+    
+} 
+
+function closeAddDialog() {
+    let d = document.getElementById('addModal')
+    d.style.display = "none"
+    d.close()
+}
+
+function closeDeleteDialog() {
+    let d = document.getElementById('deleteModal')
+    d.style.display = "none"
+    d.close()
 }
